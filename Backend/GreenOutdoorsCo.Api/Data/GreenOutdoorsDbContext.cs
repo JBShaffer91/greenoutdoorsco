22df6c43447f745f7f2bffe9bf083feaca69df8c
Justin Shaffer <justinbshaffer91@gmail.com>
2023-06-11 10:02:28 -0700

    Refactor DbContext and UserService to handle nullable reference types
    This commit does the following:
    
    - Creates GreenOutdoorsDbContext for Entity Framework Core.
    - Updates UserService to use the newly created DbContext.
    - Refactors the GreenOutdoorsDbContext.cs to ensure DbSet properties are non-nullable.
    - Updates UserService.cs to use a non-nullable DbContext, removing the need for nullable warning suppression.
    - Updates UserService.cs and GreenOutdoorsDbContext.cs to use 2 spaces for indentation for better readability.
    
    These changes help us follow best practices for nullable reference types, ensure consistent code formatting, and set up our DbContext for database interactions.

--- Backend/GreenOutdoorsCo.Api/Data/GreenOutdoorsDbContext.cs
+++ Backend/GreenOutdoorsCo.Api/Data/GreenOutdoorsDbContext.cs
@@ -1,18 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using GreenOutdoorsCo.Api.Models;
+using System;
 
 namespace GreenOutdoorsCo.Api.Data
 {
-    public class GreenOutdoorsDbContext : DbContext
-    {
-        public DbSet<User>? Users { get; set; }
-        public DbSet<Product>? Products { get; set; }
-        public DbSet<Order>? Orders { get; set; }
-        public DbSet<OrderItem>? OrderItems { get; set; }
+  public class GreenOutdoorsDbContext : DbContext
+  {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySql("server=localhost;database=greenoutdoorsco;user=root;", new MySqlServerVersion(new Version(8, 0, 33)));
-        }
+    public GreenOutdoorsDbContext(DbContextOptions<GreenOutdoorsDbContext> options) : base(options)
+    {
+      Users = Set<User>();
+      Products = Set<Product>();
+      Orders = Set<Order>();
+      OrderItems = Set<OrderItem>();
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseMySql("server=localhost;database=greenoutdoorsco;user=root;", new MySqlServerVersion(new Version(8, 0, 33)));
+    }
+  }
 }
--- Backend/GreenOutdoorsCo.Api/Services/UserService.cs
+++ Backend/GreenOutdoorsCo.Api/Services/UserService.cs
@@ -1,28 +1,47 @@
 using System;
 using System.Threading.Tasks;
-using GreenOutdoorsCo.Api.Models;
+using GreenOutdoorsCo.Api.Data;
 using GreenOutdoorsCo.Api.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 namespace GreenOutdoorsCo.Api.Services
 {
-    public class UserService : IUserService
+  public class UserService : IUserService
+  {
+    private readonly GreenOutdoorsDbContext _context;
+    
+    public UserService(GreenOutdoorsDbContext context) 
     {
-        public Task<ServiceResponse<int>> Register(UserRegisterDto user, string password)
-        {
-            throw new NotImplementedException();
-            // Implement your registration logic here
-        }
-
-        public Task<ServiceResponse<string>> Login(string username, string password)
-        {
-            throw new NotImplementedException();
-            // Implement your login logic here
-        }
-
-        // public Task<ServiceResponse<string>> Logout()
-        // {
-        //     throw new NotImplementedException();
-        //     // Implement your logout logic here
-        // }
+      _context = context;
     }
+
+    public async Task<ServiceResponse<int>> Register(UserRegisterDto user, string password)
+    {
+      // You can now use the _context to interact with your database
+      if (await _context.Users.AnyAsync(u => u.Username == user.Username)) // <- Using _context to check if a user exists in the database
+      {
+        throw new Exception("User already exists");
+      }
+      // Implement your registration logic here
+      throw new NotImplementedException();
+    }
+
+    public async Task<ServiceResponse<string>> Login(string username, string password)
+    {
+      // You can also use _context here to check the user's credentials against the database
+      var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+      if (user == null)
+      {
+        throw new Exception("User not found");
+      }
+      // Implement your login logic here
+      throw new NotImplementedException();
+    }
+
+    // public Task<ServiceResponse<string>> Logout()
+    // {
+    //     throw new NotImplementedException();
+    //     // Implement your logout logic here
+    // }
+  }
 }
